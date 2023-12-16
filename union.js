8d83204f30a07238cb92e1c9bcf74a7ad95225e3
Md Rashed Ali <tools.rashed@gmail.com>
2023-12-16 12:22:00 +0600

    explore unions

---
+++ union.js
@@ -0,0 +1,8 @@
+var user = {
+    name: "Rashed",
+    id: 202
+};
+//Unions In Arrays
+var combineArray = ["1", 1];
+var userRole;
+userRole = "Mod";
---
+++ union.ts
@@ -0,0 +1,23 @@
+type User = {
+    name: string,
+    id: number
+}
+
+type Admin ={
+    username: string,
+    id: number
+}
+
+
+const user : Admin | User = {
+    name: "Rashed",
+    id: 202
+}
+
+
+//Unions In Arrays
+const combineArray:(string |number)[] = ["1", 1]
+
+let userRole: "Admin"| "User" | "Mod"
+
+userRole = "Mod"
