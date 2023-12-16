c2be7a969df8795e3c0c57dd75d2fe9bfea2c030
Md Rashed Ali <tools.rashed@gmail.com>
2023-12-16 18:31:32 +0600

    class and constractors

--- dist/index.js
+++ dist/index.js
@@ -2,3 +2,12 @@
 console.log("Welcome to type script");
 let checkConsole = "TypeScript is in the console";
 console.log(checkConsole);
+//Constructor || Class
+class User {
+    constructor(name, email, id) {
+        this.name = name;
+        this.email = email;
+        this.id = id;
+    }
+}
+const activeUser = new User("Rashed", "Rashed@gmail.com", 202);
--- index.ts
+++ index.ts
@@ -1,3 +1,11 @@
-console.log("Welcome to type script")
-let checkConsole = "TypeScript is in the console"
-console.log(checkConsole)
+console.log("Welcome to type script");
+let checkConsole = "TypeScript is in the console";
+console.log(checkConsole);
+
+//Constructor || Class
+
+class User {
+  constructor(public name: string, public email: string, public id: number) {}
+}
+
+const activeUser = new User("Rashed", "Rashed@gmail.com", 202);
