a6766e0cc181e9c7b921872e38daff33bfcd0ce3
Md Rashed Ali <tools.rashed@gmail.com>
2023-12-16 10:04:09 +0600

    readonly keyword

--- typeAlias.ts
+++ typeAlias.ts
@@ -10,4 +10,20 @@ return {name: "Rashed", email: "email@email.com", isPaid: true}
 }
 
 
+//Read Only Keyword
+type MongoUser = {
+    readonly _id: string,
+    name: string,
+    email: string
+}
+
+let user: MongoUser = {
+    _id: "1234",
+    name: "rashed",
+    email: "string"
+}
+
+user._id = "12"
+
+
 export{}
